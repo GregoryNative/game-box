b8148cf349f458794cc30a478ed5896846f0e944
Gregory <gregorygalushka@gmail.com>
2020-04-22 17:00:40 +0300

    Updates

---
+++ precache-manifest.7767cff686d445df4be091bc737790bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46133b23d87622b72931bfdf5321e44c",
+    "url": "/game-box/index.html"
+  },
+  {
+    "revision": "b40fcdf3543ae01513da",
+    "url": "/game-box/static/css/main.744b0c48.chunk.css"
+  },
+  {
+    "revision": "90071d7a7ad63e001920",
+    "url": "/game-box/static/js/2.3e311fd5.chunk.js"
+  },
+  {
+    "revision": "1b730b11501651dc0ff37e34a614c377",
+    "url": "/game-box/static/js/2.3e311fd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b40fcdf3543ae01513da",
+    "url": "/game-box/static/js/main.cb8b17cb.chunk.js"
+  },
+  {
+    "revision": "f827bb48688de28599e4",
+    "url": "/game-box/static/js/runtime-main.009d4889.js"
+  }
+]);
